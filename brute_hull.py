c7b5c5a941fc29d4ccc5a05caa0b10abdf12b7ab
aaroncs <aachansy2007@gmail.com>
2018-11-07 18:51:49 -0800

    Remove closest pair function

--- brute_hull.py
+++ brute_hull.py
@@ -1,15 +1,2 @@
-import sys
-import math
-
-
-def closest_pair(data_set):
-    dist = []
-    for p1 in data_set:
-        for p2 in data_set:
-            if p1 is not p2:
-                dist.append(math.sqrt(p1**2 + p2**2))
-    return min(dist)
-
-
 def brute_hull(data_set):
     print("Test")
