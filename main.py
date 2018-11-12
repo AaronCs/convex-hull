cdc43b8e1fd84cdd3d29801658bf88e9fe457cfe
Aaron <aachansy2007@gmail.com>
2018-11-12 13:25:27 -0800

    Fixes brutehull bugs
    Omg it just wasn't working because the formula for a is y2-y1 not y1-y2

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # CS 350 Project
 # Reference: Introduction to The Design and Analysis of Algorithms by Anany Levitin
 
+from random import randint
+import matplotlib.pyplot as plt
+
 
 def brute_hull(data_set):
     """
@@ -14,30 +17,35 @@ def brute_hull(data_set):
 
     convex_hull = []
     for p1 in data_set:
-        for p2 in data_set:
+        for p2 in data_set[data_set.index(p1) + 1:]:
             if p2 != p1:
                 # ax + by = c
                 # c = x1y2 - x2y1
-                in_set = True
-                a = p1[1] - p2[1]
-                b = p1[0] - p2[0]
-                c = (p1[0] * p2[1]) - (p1[1] * p1[0])
-                signs = []
+
+                x1 = p1[0]
+                y1 = p1[1]
+
+                x2 = p2[0]
+                y2 = p2[1]
+
+                a = y2 - y1
+                b = x1 - x2
+                c = (x1 * y2) - (y1 * x2)
+                pos = 0
+                neg = 0
 
                 for p3 in data_set:
-                    if (p3 != p1) and (p3 != p2):
+                    if p3 != p1 and p3 != p2:
                         # Check if ax+by-c has same sign for all points.
-                        signs.append((a * p3[0]) + (b * p3[1]) - c)
+                        lin_eq = (a * p3[0]) + (b * p3[1]) - c
+                        if lin_eq >= 0:
+                            pos += 1
+                        if lin_eq <= 0:
+                            neg += 1
 
-                pos = False
-                neg = False
-                for sign in signs:
-                    if sign > 0:
-                        pos = True
-                    if sign < 0:
-                        neg = True
-
-                if not (pos and neg):
+                # -2 b/c we ignore the points made by the line.
+                if pos == (len(data_set) - 2) or neg == (len(data_set) - 2):
+                    convex_hull.append(p1)
                     convex_hull.append(p2)
     return convex_hull
 
@@ -46,9 +54,35 @@ def gift_wrap(data_set):
     pass
 
 
+def gen_data(num_points, min, max):
+    """
+    :return: randomly generates a point.
+    """
+    data_set = set()
+    x = randint(min, max)
+    y = randint(min, max)
+
+    while num_points:
+        data_set.add((x, y))
+        x = randint(min, max)
+        y = randint(min, max)
+        while (x, y) in data_set:
+            x = randint(min, max)
+            y = randint(min, max)
+        num_points -= 1
+    return data_set
+
+
 def main():
-    data_set = []
-    brute_hull(data_set)
+    data_count = 5
+    rand_min, rand_max = 0, 250
+    # data_set = list(gen_data(data_count, rand_min, rand_max))
+    data_set = [(0, 0), (1, 2), (0, 4), (2, 2)]
+    convex_hull = brute_hull(data_set)
+    print(convex_hull)
+    plt.scatter(*zip(*data_set))
+    plt.plot(*zip(*convex_hull))
+    plt.show()
 
 
 if __name__ == "__main__":
